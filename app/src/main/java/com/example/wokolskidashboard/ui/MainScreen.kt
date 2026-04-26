c399104de1e04e4b630c1cebded444494ef6b092
Janek <jan.bulkowski@cyfroweszkoly.pl>
2026-04-26 21:33:16 +0200

    postęp w pracy

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,30 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.wokolskidashboard.model.Transaction
+import com.example.wokolskidashboard.ui.components.*
+import com.example.wokolskidashboard.ui.theme.WokolskiDashBoardTheme
+
+@Composable
+fun MainScreen() {
+    val transactions = remember { mutableStateListOf<Transaction>() }
+    val balance = transactions.sumOf { if (it.isExpence) -it.kwota else it.kwota }
+
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            BalanceHeader(balance = balance)
